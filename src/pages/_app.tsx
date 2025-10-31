21e489b488d40045112036921ba5227726bb852a
meseguraaa <brunoleal2005@yahoo.com.br>
2025-10-30 22:42:06 -0300

    Inserindo metadata no projeto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,25 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-sans",
   display: "swap",
 });
+
+export const metadata: Metadata = {
+  title: "Bruno Leal | Links",
+  description: "Meu linktree pessoal com todos os meus links e redes sociais.",
+  openGraph: {
+    title: "Bruno Leal | Links",
+    description: "Acesse todos os meus links e redes sociais em um só lugar.",
+    images: [
+      "https://images.prismic.io/linktree/aQPDr7pReVYa32qp_IMG_3332.JPG?auto=format,compress",
+    ],
+  },
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
